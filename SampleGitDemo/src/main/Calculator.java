3e2e4b75258d121c843738a315dff78e728a8258
ADMIN <ADMIN@DESKTOP-9AOO5P6.lan>
2021-03-23 11:02:05 +0530

    masterfunc

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -27,7 +27,7 @@ public class Calculator {
 	public static void masterfunc1(int num1, int num2){
 		System.out.println(num1-num2);
 	}
-	public static void masterfunc2(int num1, int num2){
+	public static void masterfunc(int num1, int num2){
 		System.out.println(num1-num2);
 	}
 	
