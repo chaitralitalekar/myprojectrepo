d1e9411a49718f8ec9c15ad76faadd2727c0de8a
ADMIN <ADMIN@DESKTOP-9AOO5P6.lan>
2021-03-23 10:54:53 +0530

    masterfunc1 added

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -23,4 +23,9 @@ public class Calculator {
 	public static void sqr(int num1){
 		System.out.println(num1*num1);
 	}
+	
+	public static void masterfunc1(int num1, int num2){
+		System.out.println(num1-num2);
+	}
+	
 }
